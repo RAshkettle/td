a5571e920d6db78a6ec56d08fd137cb0cf0e2540
Richard Ashkettle <richard.ashkettle@gmail.com>
2024-12-09 18:00:36 -0500

    added cobra for cli

---
+++ cmd/completion.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func completionCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "completion",
+		Short: "Generate the autocompletion script for the specified shell",
+	}
+}
+
+func init() {
+	completion := completionCommand()
+
+	// mark completion hidden
+	completion.Hidden = true
+	rootCmd.AddCommand(completion)
+
+	// other subcommands
+}
---
+++ cmd/root.go
@@ -0,0 +1,39 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "td",
+	Short: "command line tool for task lists",
+	Long: `TD is a CLI application for managing tasks.  It is a simple
+  tool created to manage the day to day activities of a project.`,
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.td.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
